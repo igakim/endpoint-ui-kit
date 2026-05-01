813ee6821148c86160d2d168699f88fca811624a
mammoth90 <mamontov.max90@gmail.com>
2026-05-01 17:43:34 +0500

    button/ primary, secondary

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,91 @@
 import './App.css'
 import Button from './components/Button';
 import styles from './App.module.scss';
+import { AiOutlineSelect } from "react-icons/ai";
 
 
 function App() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.section}>
         <h2>Buttons</h2>
+      <div className={styles.section}>
+          <div className={styles.singleButton}>
         <Button
-          onClick={() => console.log('Clicked')}
-          text="Hello"
+          onClick={console.log('Clicked')}
+          text="Primary"
         />
+      </div>
+          <div className={styles.singleButton}>
         <Button
           onClick={() => console.log('Clicked')}
-          text="Hello"
+          text="Secondary"
           variant="secondary"
         />
+    </div>
+          <div className={styles.singleButton}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          text="Disabled"
+          isDisabled={true}
+        />
+    </div>
+          <div className={styles.singleButton}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          text="Text"
+          isLoading={true}
+        />
+    </div>
+          <div className={styles.singleButton}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          text="Hello"
+          isLoading={true}
+          loadingText='Loading'
+        />
+    </div>
+    <div className={`${styles.singleButton} ${styles.block}`}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          text="Block"
+          variant="secondary"
+          block={true}
+        />
+    </div>
+    <div className={styles.singleButton}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          text="Sized 100x100"
+          variant="secondary"
+          size={{width: 100, height: 100}}
+        />
+    </div>
+    <div className={styles.singleButton}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          text="configs"
+          variant="secondary"
+          icon={AiOutlineSelect}
+        />
+    </div>
+    <div className={styles.singleButton}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          text="logo"
+          iconPosition='left'
+          variant="secondary"
+          icon={AiOutlineSelect}
+        />
+    </div>
+    <div className={styles.singleButton}>
+        <Button
+          onClick={() => console.log('Clicked')}
+          variant="secondary"
+          icon={AiOutlineSelect}
+          onlyIcon={true}
+        />
+    </div>
       </div>
     </div>
   )
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,27 +1,54 @@
 import React from 'react';
 import cn from 'classnames';
 import styles from './Button.module.scss';
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Button = ({
   type = 'button',
+  iconPosition = 'right',
   onClick,
   text = '',
+  isDisabled = false,
   variant = 'primary',
+  isLoading = false,
+  loadingText = null,
+  block = false,
+  size = false,
+  icon = null,
+  onlyIcon = false,
 }) => {
   const buttonStyles = cn({
     [styles.button]: true,
     [styles.buttonPrimary]: variant === 'primary',
     [styles.buttonSecondary]: variant === 'secondary',
+    [styles.block]: block,
+    [styles.sized]: size,
+    [styles.onlyIcon]: onlyIcon,
   });
-
+  const iconStyle = cn({
+    [styles.icon]: true,
+    [styles.Loading]: isLoading,    
+    [styles.iconPositionRight]: iconPosition === 'right',
+    [styles.iconPositionLeft]: iconPosition === 'left',
+    [styles.onlyIcon]: onlyIcon 
+  })
+  const Icon = icon
   return (
     <button
       type={type}
       onClick={onClick}
+      disabled={isDisabled || isLoading}
       className={buttonStyles}
+      style={{
+              width: size.width,
+              height: size.height
+      }}
     >
-      <span>{text}</span>
-    </button>
+      {icon && iconPosition === 'left' && <Icon className={iconStyle}/>} 
+      <span>{loadingText || text}</span> 
+      {icon && iconPosition === 'right' && <Icon className={iconStyle}/>} 
+      {isLoading &&  <AiOutlineLoading3Quarters className={iconStyle} />}
+     </button>
   );
 };
 
