ecb67d8e60568159708781c70ac87b2be01a2e15
Igor Kim <igkim90@gmail.com>
2026-05-04 17:33:19 +0500

    add: modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 import Button from './components/Button';
 import styles from './App.module.scss';
+import Modal from './components/Modal/index.js';
+import { useState } from 'react';
 
 
 function App() {
+  const [modals, setModals] = useState({
+    simpleOpen: false,
+  });
 
   return (
     <div className={styles.container}>
@@ -18,6 +23,22 @@ function App() {
           text="Hello"
           variant="secondary"
         />
+      </div>
+
+      <div className={styles.section}>
+        <button type="button" onClick={() => setModals({ ...modals, simpleOpen: true })}>Open modal</button>
+        <Modal
+          dismissible={false}
+          isOpen={modals.simpleOpen}
+          onClose={() => setModals({ ...modals, simpleOpen: false })}
+        >
+          <h2>Modal</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus in ipsa laboriosam sequi? Ab at dignissimos fugiat iusto laudantium maiores, modi molestias provident quibusdam quo, saepe soluta tempore totam voluptatum?</p>
+          <div>
+            <button type="button">Close</button>
+            <button type="button">Ok</button>
+          </div>
+        </Modal>
       </div>
     </div>
   )
